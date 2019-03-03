7835c04d228b64698c1a33ed5dfeae2d52730e25
cccczl <cccczl@hotmail.com>
2019-03-04 04:35:23 +0800

    09.3.4

---
+++ day3/1.py
@@ -0,0 +1,13 @@
+list1 = [1, 4, 6, 7, 3, 5, 2, 8, 38, 3, 46, 78]
+list1 = set(list1)
+
+list2 = set([2, 4, 5, 6, 7, 9, 10, 11, 22])
+print(list1, list2)
+# 交集
+print(list1.intersection(list2))
+# 并集
+print(list1.union(list2))
+# 差集
+print(list1.difference(list2))
+
+print(type(list1))
