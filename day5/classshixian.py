28aea0704b7a3d588234876101df3936501544fa
cccczl <cccczl@hotmail.com>
2019-12-08 13:36:59 +0800

    编辑了部分三级菜单代码

---
+++ day5/classshixian.py
@@ -0,0 +1,23 @@
+class shuiguo():
+
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+
+    def shelf_life(self):
+
+        print(self.name.title() + " this is 7 day")
+
+    def shuiguo_print(self):
+
+        print(self.name, self.color)
+
+    def __len__(self):
+        return len(self.name)
+
+my_shuiguo = shuiguo('apple' 'orangor', 'red' 'blue')
+my_shuiguo.shelf_life()
+my_shuiguo.shuiguo_print()
+print(len(my_shuiguo))
